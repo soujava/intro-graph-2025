2638ba0f85457ac95b154b46f2230725f2c4bd4b
Otavio Santana <otaviopolianasantana@gmail.com>
2025-05-21 03:54:09 +0100

    feat: update neo4j update onrepository
    Signed-off-by: Otavio Santana <otaviopolianasantana@gmail.com>

---
+++ neo4j/src/main/java/org/jnosql/demo/se/BookRepository.java
@@ -0,0 +1,26 @@
+package org.jnosql.demo.se;
+
+
+import jakarta.data.repository.Param;
+import jakarta.data.repository.Repository;
+import org.eclipse.jnosql.databases.neo4j.mapping.Neo4JRepository;
+import org.eclipse.jnosql.databases.tinkerpop.mapping.Gremlin;
+import org.eclipse.jnosql.databases.tinkerpop.mapping.TinkerPopRepository;
+
+import java.util.List;
+import java.util.Optional;
+
+@Repository
+public interface BookRepository extends Neo4JRepository<Book, Long> {
+
+    Optional<Book> findByName(String name);
+
+    @Gremlin("g.V().hasLabel('Book').out('is').hasLabel('Category').has('name','Architecture').in('is').dedup()")
+    List<Book> findArchitectureBooks();
+
+    @Gremlin("g.E().hasLabel('is').has('relevance', gte(9)).outV().hasLabel('Book').dedup()")
+    List<Book> highRelevanceBooks();
+
+    @Gremlin("g.V().hasLabel('Book').has('name', @name)")
+    List<Book> queryByName(@Param("name") String name);
+}
