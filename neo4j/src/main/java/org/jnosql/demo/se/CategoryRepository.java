5f3cef37c77f75826e922444d676d0b6215085fa
Otavio Santana <otaviopolianasantana@gmail.com>
2025-05-21 03:56:45 +0100

    feat: update import on neo4
    Signed-off-by: Otavio Santana <otaviopolianasantana@gmail.com>

--- neo4j/src/main/java/org/jnosql/demo/se/CategoryRepository.java
+++ neo4j/src/main/java/org/jnosql/demo/se/CategoryRepository.java
@@ -2,6 +2,8 @@ package org.jnosql.demo.se;
 
 
 import jakarta.data.repository.Repository;
+import org.eclipse.jnosql.databases.neo4j.mapping.Cypher;
+import org.eclipse.jnosql.databases.neo4j.mapping.Neo4JRepository;
 import org.eclipse.jnosql.databases.tinkerpop.mapping.Gremlin;
 import org.eclipse.jnosql.databases.tinkerpop.mapping.TinkerPopRepository;
 
@@ -9,10 +11,10 @@ import java.util.List;
 import java.util.Optional;
 
 @Repository
-public interface CategoryRepository extends TinkerPopRepository<Category, Long> {
+public interface CategoryRepository extends Neo4JRepository<Category, Long> {
 
     Optional<Category> findByName(String name);
 
-    @Gremlin("g.V().hasLabel('Category').where(__.in('is').count().is(gt(1)))")
+    @Cypher("g.V().hasLabel('Category').where(__.in('is').count().is(gt(1)))")
     List<Category> commonCategories();
 }
