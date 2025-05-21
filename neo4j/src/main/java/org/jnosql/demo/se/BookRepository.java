d2beb95b5ac087f0e100789750f40bc3234a8d0d
Otavio Santana <otaviopolianasantana@gmail.com>
2025-05-21 03:54:35 +0100

    feat: update imports at book repository on neo4j
    Signed-off-by: Otavio Santana <otaviopolianasantana@gmail.com>

--- neo4j/src/main/java/org/jnosql/demo/se/BookRepository.java
+++ neo4j/src/main/java/org/jnosql/demo/se/BookRepository.java
@@ -3,9 +3,8 @@ package org.jnosql.demo.se;
 
 import jakarta.data.repository.Param;
 import jakarta.data.repository.Repository;
+import org.eclipse.jnosql.databases.neo4j.mapping.Cypher;
 import org.eclipse.jnosql.databases.neo4j.mapping.Neo4JRepository;
-import org.eclipse.jnosql.databases.tinkerpop.mapping.Gremlin;
-import org.eclipse.jnosql.databases.tinkerpop.mapping.TinkerPopRepository;
 
 import java.util.List;
 import java.util.Optional;
@@ -15,12 +14,12 @@ public interface BookRepository extends Neo4JRepository<Book, Long> {
 
     Optional<Book> findByName(String name);
 
-    @Gremlin("g.V().hasLabel('Book').out('is').hasLabel('Category').has('name','Architecture').in('is').dedup()")
+    @Cypher("g.V().hasLabel('Book').out('is').hasLabel('Category').has('name','Architecture').in('is').dedup()")
     List<Book> findArchitectureBooks();
 
-    @Gremlin("g.E().hasLabel('is').has('relevance', gte(9)).outV().hasLabel('Book').dedup()")
+    @Cypher("g.E().hasLabel('is').has('relevance', gte(9)).outV().hasLabel('Book').dedup()")
     List<Book> highRelevanceBooks();
 
-    @Gremlin("g.V().hasLabel('Book').has('name', @name)")
+    @Cypher("g.V().hasLabel('Book').has('name', @name)")
     List<Book> queryByName(@Param("name") String name);
 }
