3ecdd663c037af1fd963b7c1eb368c8f8b461dbb
Otavio Santana <otaviopolianasantana@gmail.com>
2025-05-21 04:03:27 +0100

    feat: update book app
    Signed-off-by: Otavio Santana <otaviopolianasantana@gmail.com>

--- neo4j/src/main/java/org/jnosql/demo/se/BookApp.java
+++ neo4j/src/main/java/org/jnosql/demo/se/BookApp.java
@@ -26,43 +26,41 @@ public final class BookApp {
     public static void main(String[] args) {
 
         try (SeContainer container = SeContainerInitializer.newInstance().initialize()) {
-            Neo4JTemplate graph = container.select(Neo4JTemplate.class).get();
-            BookService service = container.select(BookService.class).get();
+            var template = container.select(Neo4JTemplate.class).get();
+            var service = container.select(BookService.class).get();
 
-            Category software = service.save(Category.of("Software"));
-            Category java = service.save(Category.of("Java"));
-            Category architecture = service.save(Category.of("Architecture"));
-            Category performance = service.save(Category.of("Performance"));
+            var software = service.save(Category.of("Software"));
+            var java = service.save(Category.of("Java"));
+            var architecture = service.save(Category.of("Architecture"));
+            var performance = service.save(Category.of("Performance"));
 
-            Book effectiveJava = service.save(Book.of("Effective Java"));
-            Book cleanArchitecture = service.save(Book.of("Clean Architecture"));
-            Book systemDesign = service.save(Book.of("System Design Interview"));
-            Book javaPerformance = service.save(Book.of("Java Performance"));
+            var effectiveJava = service.save(Book.of("Effective Java"));
+            var cleanArchitecture = service.save(Book.of("Clean Architecture"));
+            var systemDesign = service.save(Book.of("System Design Interview"));
+            var javaPerformance = service.save(Book.of("Java Performance"));
 
-
-            graph.edge(Edge.source(effectiveJava).label("is").target(java).property("relevance", 10).build());
-            graph.edge(Edge.source(effectiveJava).label("is").target(software).property("relevance", 9).build());
-            graph.edge(Edge.source(cleanArchitecture).label("is").target(software).property("relevance", 8).build());
-            graph.edge(Edge.source(cleanArchitecture).label("is").target(architecture).property("relevance", 10).build());
-            graph.edge(Edge.source(systemDesign).label("is").target(architecture).property("relevance", 9).build());
-            graph.edge(Edge.source(systemDesign).label("is").target(software).property("relevance", 7).build());
-            graph.edge(Edge.source(javaPerformance).label("is").target(performance).property("relevance", 8).build());
-            graph.edge(Edge.source(javaPerformance).label("is").target(java).property("relevance", 9).build());
-
+            template.edge(Edge.source(effectiveJava).label("is").target(java).property("relevance", 10).build());
+            template.edge(Edge.source(effectiveJava).label("is").target(software).property("relevance", 9).build());
+            template.edge(Edge.source(cleanArchitecture).label("is").target(software).property("relevance", 8).build());
+            template.edge(Edge.source(cleanArchitecture).label("is").target(architecture).property("relevance", 10).build());
+            template.edge(Edge.source(systemDesign).label("is").target(architecture).property("relevance", 9).build());
+            template.edge(Edge.source(systemDesign).label("is").target(software).property("relevance", 7).build());
+            template.edge(Edge.source(javaPerformance).label("is").target(performance).property("relevance", 8).build());
+            template.edge(Edge.source(javaPerformance).label("is").target(java).property("relevance", 9).build());
 
             System.out.println("\n📚 Books in 'Architecture' category:");
-            var architectureBooks = graph.cypher(  "MATCH (b:Book)-[:is]->(c:Category {name: 'Architecture'}) RETURN b AS book", Collections.emptyMap()).toList();
+            var architectureBooks = template.cypher(  "MATCH (b:Book)-[:is]->(c:Category {name: 'Architecture'}) RETURN b AS book", Collections.emptyMap()).toList();
             architectureBooks.forEach(doc -> System.out.println(" - " + doc));
 
             System.out.println("\n🔍 Categories with more than one book:");
-            var commonCategories = graph.cypher( "MATCH (b:Book)-[:is]->(c:Category) " +
+            var commonCategories = template.cypher( "MATCH (b:Book)-[:is]->(c:Category) " +
                             "WITH c, count(b) AS total " +
                             "WHERE total > 1 " +
                             "RETURN c"
                     , Collections.emptyMap()).toList();
             commonCategories.forEach(doc -> System.out.println(" - " + doc));
 
-            var highRelevanceBooks = graph.cypher(   "MATCH (b:Book)-[r:is]->(:Category) " +
+            var highRelevanceBooks = template.cypher(   "MATCH (b:Book)-[r:is]->(:Category) " +
                     "WHERE r.relevance >= 9 " +
                     "RETURN DISTINCT b", Collections.emptyMap()).toList();
 
@@ -70,7 +68,7 @@ public final class BookApp {
             highRelevanceBooks.forEach(doc -> System.out.println(" - " + doc));
 
             System.out.println("\n📚 Books with name: 'Effective Java':");
-            var effectiveJavaBooks = graph.cypher("MATCH (b:Book {name: $name}) RETURN b", Collections.singletonMap("name", "Effective Java")
+            var effectiveJavaBooks = template.cypher("MATCH (b:Book {name: $name}) RETURN b", Collections.singletonMap("name", "Effective Java")
             ).toList();
             effectiveJavaBooks.forEach(doc -> System.out.println(" - " + doc));
         }
